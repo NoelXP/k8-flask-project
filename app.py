ea2aad174ac0a219862b279dec690779048b3e60
noelxp <Noel@clix.com>
2023-06-06 22:28:51 -0500

    added port to flask app

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Simple Flask application'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
